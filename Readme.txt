37bc5966fa181c4235f3ab8a0939d24a7a295500
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2022-11-25 07:33:49 +0530

    Added the toggle support for Markdown editor

--- Readme.txt
+++ Readme.txt
@@ -11,3 +11,7 @@ npm install bulmaswatch --legacy-peer-deps
 npm install --save-exact monaco-jsx-highlighter@0.0.15 jscodeshift@0.11.0 @types/jscodeshift@0.7.2  --legacy-peer-deps
 
 npm install --save-exact react-resizable@3.0.4 @types/react-resizable@3.0.2 --legacy-peer-deps
+
+# Mark down editor
+# @uiw/react-md-editor
+npm install --save-exact @uiw/react-md-editor@2.1.1
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
-import CodeCell from './components/code-cell/code-cell';
+// import CodeCell from './components/code-cell/code-cell';
+import TextEditor from './components/text-cell/text-editor/text-editor';
 
 
 const App = () => {    
 
     return (
         <div>
-            <CodeCell />
-            
+            {/* <CodeCell /> */}
+            <TextEditor />
         </div>
     );
 }
